9089b4f6d198c1cc79f6fc23225911c1d6f0746e
Ishtiaq Mahmood Amin <ishtiaq.mahmood@protonmail.com>
2021-11-17 20:52:24 +0100

    Custom map view

--- app/src/main/java/ishtiaq/codingchallenge/carsmaplist/MainActivity.kt
+++ app/src/main/java/ishtiaq/codingchallenge/carsmaplist/MainActivity.kt
@@ -2,10 +2,27 @@ package ishtiaq.codingchallenge.carsmaplist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MapStyleOptions
+import ishtiaq.codingchallenge.carsmaplist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding // after wrapping with <layout> make sure to build
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.carmap.getFragment<SupportMapFragment>().getMapAsync(this)
     }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style))
+
+    }
+
 }
